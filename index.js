28085a7fc7bf2a101beddc3b25bc81ad6a2cfb1f
Nemerd <xtremestripedtiger@gmail.com>
2022-10-12 19:44:34 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+class Usuario {
+
+    constructor(nombre, apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        libros = [];
+        mascotas = [];
+    }
+    
+    getFullName(){
+        
+    }
+
+    addMascota(){
+        
+    }
+
+    countMascotas(){
+        
+    }
+
+    addBook(){
+        
+    }
+
+    getBookNames(){
+        
+    }
+
+  }
