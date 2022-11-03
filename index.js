8b6f3f2dcae712d01a9d8b2c69bc954c8642929e
Nemerd <xtremestripedtiger@gmail.com>
2022-11-02 21:34:47 -0300

    Desafío API RESTful

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const express = require("express");
+const { response } = require("express");
 const PORT = 8080;
 
 
@@ -7,15 +8,16 @@ class Contenedor{
     
     constructor(route) {
         this.route = route;
+        // Chequear si existe, si no crearla
         if (!fs.existsSync(this.route)){
             fs.writeFileSync(this.route, JSON.stringify([]), error => console.log(error))
         }
         this.encoding = 'utf-8';
-        this.lastId = 0;
+        this.lastId = 3;
     }
     
     save(obj){
-        /* Recibe un objeto, lo guarda en el archivo, devuelve el id asignado. */
+        // Recibe un objeto, lo guarda en el archivo, devuelve el id asignado.
         let currentID = this.lastId +=1
         const newObj = { ...obj, id: currentID };
         
@@ -31,12 +33,12 @@ class Contenedor{
 
     
     getById(num){
-        /* Recibe un id y devuelve el objeto con ese id, o null si no está. */
+        // Recibe un id y devuelve el objeto con ese id, o null si no está.
         try {
             let listOfObjects = []
             const file = fs.readFileSync(this.route, this.encoding)
             listOfObjects = [...JSON.parse(file)]
-            return listOfObjects.find(i => i.id === num) || null; /* Object */
+            return listOfObjects.find(i => i.id === parseInt(num)) || null; /* Object */
         } catch (error) {
             console.log(`Ocurió un error en Contenedor.getById: ${error}`);
             return null;
@@ -44,7 +46,7 @@ class Contenedor{
     }
 
     getAll(){
-        /* Devuelve un array con los objetos presentes en el archivo. */
+        // Devuelve un array con los objetos presentes en el archivo.
         try {
             return [...JSON.parse(fs.readFileSync(this.route, this.encoding))]; /* Object[] */
         } catch (error) {
@@ -53,11 +55,11 @@ class Contenedor{
     }
 
     deleteById(num){
-        /* Elimina del archivo el objeto con el id buscado. */
+        // Elimina del archivo el objeto con el id buscado.
         try {
             const file = fs.readFileSync(this.route, this.encoding);
             const listOfObjects = JSON.parse(file);
-            const deleteIndex = listOfObjects.findIndex(i => i.id === num);
+            const deleteIndex = listOfObjects.findIndex(i => i.id === parseInt(num));
             listOfObjects.splice(deleteIndex, 1);
             fs.writeFileSync(this.route, JSON.stringify(listOfObjects));
         } catch (error) {
@@ -66,29 +68,74 @@ class Contenedor{
     }
 
     deleteAll(){
-        /* Elimina todos los objetos presentes en el archivo. */
+        // Elimina todos los objetos presentes en el archivo.
         try {
             fs.writeFileSync(this.route, JSON.stringify([]));
         } catch (error) {
             console.log(`Ocurió un error en Contenedor.deleteAll: ${error}`);
         }
     }
+
+    updateById(id, update){
+        // Recibe una actualización y la aplica
+
+        // Traemos el objeto y lo actualizamos
+        let obj = this.getById(id);
+        obj = { ...obj, ...update };
+
+        // Abrimos el archivo y buscamos el index del elememento que tenemos que actualizar
+        let file = [...JSON.parse(fs.readFileSync(this.route, this.encoding))];
+        const modifyIndex = file.findIndex((i) => i.id === parseInt(id));
+        // Modificamos el array que nos sirve de base de datos y escribimos las modificaciones
+        file[modifyIndex] = obj;
+        fs.writeFileSync(this.route, JSON.stringify(file));
+
+        return obj;
+    }
 }
 const container = new Contenedor("./productos.json");
 
 // Desafío 6
 const server = express();
-
-server.get("/productos", (request, response) => {
-    // Devolver todos los productos
-    response.send(JSON.stringify(container.getAll()));
-})
+server.use(express.json())
+server.use(express.urlencoded({ extended: true }))
 
 server.get("/productoRandom", (request, response) => {
     // Crear un ID al azar con la cantidad de elementos en el archivo
     const randomId = Math.floor(Math.random() * 3) + 1;
     // Devolver el objeto con el ID al azar
-    response.send(JSON.stringify(container.getById(randomId)));
-})
+    response.json(container.getById(randomId));
+});
+
+server.get("/api/productos", (request, response) => {
+    // Devolver todos los productos
+    response.json(container.getAll());
+});
+
+server.get("/api/productos/:id", (request, response) => {
+    // Devuelve un producto según su id
+    const { id } = request.params;
+    response.json(container.getById(id));
+});
+
+server.post("/api/productos", (request, response) => {
+    // Recibe y agrega un producto, y lo devuelve con su id asignado
+    /* container.save guarda el objeto y devuelve el id, que usamos para
+     * acceder de nuevo y mandar el objeto. */
+    response.json(container.getById(container.save(request.body)))
+});
+
+server.put("/api/productos/:id", (request, response) => {
+    // Recibe y actualiza un producto según su id
+    const { id } = request.params
+    response.json(container.updateById(id, request.body))
+});
+
+server.delete("/api/productos/:id", (request, response) => {
+    // Elimina un producto según su id
+    const { id } = request.params;
+    container.deleteById(id);
+    response.json("Deleted")
+});
 
 server.listen(PORT);
