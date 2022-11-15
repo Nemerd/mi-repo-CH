92521904cfb1fea3a7d7c1a9772011fb99c3e04b
Nemerd <xtremestripedtiger@gmail.com>
2022-11-14 21:30:02 -0300

    Ejs

---
+++ src/routers/products-ejs.js
@@ -0,0 +1,18 @@
+// Desafío Motores de plantillas
+const { Router } = require("express");
+const myRouter = Router();
+const Contenedor = require("../libs/Container");
+const fs = require("fs");
+
+const container = new Contenedor(__dirname + "/../../mock/productos.json")
+
+
+myRouter.get("/productos", (request, response) => {
+    response.render("list", { products: container.getAll() })
+});
+
+myRouter.post("/", (request, response) => {
+    response.render("main", { products: true, ...request.body })
+});
+
+module.exports = myRouter;
